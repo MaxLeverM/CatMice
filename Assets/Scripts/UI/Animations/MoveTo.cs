242852ad7f9005c873796c416833744b62265491
Ilya Kozhevnikov <77684218+Kozhevka@users.noreply.github.com>
2022-01-24 01:24:44 +0200

    Dotween/Mybox/basic UI
    MyBox for selectable inspector fields depends fron enum

---
+++ Assets/Scripts/UI/Animations/MoveTo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DG.Tweening;
+using UI;
+using UnityEngine;
+
+namespace UnityTemplateProjects.UI.Animations
+{
+    public class MoveTo : UIHelper
+    {
+        [SerializeField] private Vector3 startPosition = Vector3.zero;
+        [SerializeField] private Vector3 endPosition = Vector3.zero;
+        [SerializeField] private float timeToReachTarget = 1f;
+
+        private Vector3 targetPosition;
+
+        private void Awake()
+        {
+            targetPosition = transform.position;
+            endPosition = endPosition == Vector3.zero ? startPosition : endPosition;
+        }
+
+        [ContextMenu("MoveToTargetPosition")]
+        public void MoveToTargetPosition()
+        {
+            transform.position = startPosition;
+            transform.DOMove(targetPosition, timeToReachTarget);
+        }
+
+        [ContextMenu("MoveToEndPosition")]
+        public void MoveToEndPosition()
+        {
+            transform.DOMove(endPosition, timeToReachTarget);
+        }
+
+        public void MoveToPosition(Vector3 targetPosition, float time)
+        {
+            transform.DOMove(targetPosition, time);
+        }
+    }
+}

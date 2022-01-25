82354189f0aac6d8a0f7c3c92cbea1459a64ef18
Ilya Kozhevnikov <77684218+Kozhevka@users.noreply.github.com>
2022-01-25 21:09:47 +0200

    Revert "Revert "Merge branch 'dev' into UI""
    This reverts commit e006e004d3f9a4702ffd8f5cd39ed71558923211.

---
+++ Assets/Scripts/Networking/ISyncOnStart.cs
@@ -0,0 +1,8 @@
+﻿namespace Lever.Networking
+{
+    public interface ISyncOnStart
+    {
+        public void RaiseSync(int viewID);
+        public void OnSyncCallback(object[] customData);
+    }
+}

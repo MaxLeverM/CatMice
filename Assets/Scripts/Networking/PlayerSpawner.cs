ecc2979e358261e39afc564f8f69e9246cb4aab9
MaxLever <teampromax@gmail.com>
2022-01-26 09:38:06 +0200

    Added player spawner. Update Game scene

---
+++ Assets/Scripts/Networking/PlayerSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Lever.Networking
+{
+    public class PlayerSpawner : MonoBehaviour
+    {
+        [SerializeField] private string catPrefabName;
+        [SerializeField] private string mousePrefabName;
+        [SerializeField] private List<Transform> spawnPoints;
+        private void Start()
+        {
+            PhotonNetwork.Instantiate(catPrefabName, spawnPoints[0].position, spawnPoints[0].rotation);
+        }
+    }
+}
--- Assets/Scripts/ZenjectCustom/GameNetworkingInstaller.cs
+++ Assets/Scripts/ZenjectCustom/GameNetworkingInstaller.cs
@@ -1,13 +1,16 @@
 ﻿using Lever.Networking;
+using UnityEngine;
 using Zenject;
 
 namespace Lever.ZenjectCustom
 {
     public class GameNetworkingInstaller : MonoInstaller
     {
+        [SerializeField] private PlayerSpawner playerSpawner;
         public override void InstallBindings()
         {
             Container.Bind<GameNetworking>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
+            Container.Bind<PlayerSpawner>().FromInstance(playerSpawner).AsSingle().NonLazy();
         }
     }
 }

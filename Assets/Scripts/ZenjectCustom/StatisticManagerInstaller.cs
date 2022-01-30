092b0acb5d6dca031a7b8cd5a84d554318e12b11
MaxMironenko <mmironenko@qualium-systems.com.ua>
2022-01-30 17:44:56 +0200

    Added statstic, fix kill

---
+++ Assets/Scripts/ZenjectCustom/StatisticManagerInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Lever.Networking;
+using UnityEngine;
+using Zenject;
+
+namespace Lever.ZenjectCustom
+{
+    public class StatisticManagerInstaller : MonoInstaller
+    {
+        [SerializeField] private StatisticManager statisticManager;
+        public override void InstallBindings()
+        {
+            Container.Bind<StatisticManager>().FromInstance(statisticManager).AsSingle().NonLazy();
+        }
+    }
+}

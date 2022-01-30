c59a2dfc33e32781e6ad7a6374e45293248da97a
MaxMironenko <mmironenko@qualium-systems.com.ua>
2022-01-30 18:53:23 +0200

    Update scene

--- Assets/Scripts/Constants/SceneNames.cs
+++ Assets/Scripts/Constants/SceneNames.cs
@@ -3,6 +3,6 @@
     public static class SceneNames //Сделать конфиг файл SO, прокидывать его через Zenject
     {
         public const string Menu = "NetworkingLobby";
-        public const string Multiplayer = "NetworkingGame";
+        public const string Multiplayer = "Models 5.1";
     }
 }
